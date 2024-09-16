bb14c4893f965dbbbb9cffb6b2fb35e17a1fd5c2
Samarth Kamble <kamblesamarth04@gmail.com>
2024-09-16 23:49:39 +0530

    File Folder Struture and onboarding screen

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignIn</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignUp</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;

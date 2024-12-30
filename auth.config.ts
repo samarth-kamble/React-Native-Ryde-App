e544630527c1e8abbe9b94a4e53beed637195298
Samarth Kamble <kamblesamarth04@gmail.com>
2024-12-30 20:51:15 +0530

    Implement credential-based authentication with password validation and user retrieval

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,30 @@
-import GitHub from "next-auth/providers/github";
-
+import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
+import bcrypt from "bcrypt";
+
+import { LoginSchema } from "./schema";
+import { getUserByEmail } from "./data/user";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+
+          return null;
+        }
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;

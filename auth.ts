45a61559634c7e347f25c06da79c1e43d9c369dc
Samarth Kamble <kamblesamarth04@gmail.com>
2024-12-31 11:31:37 +0530

    Implemented user role management to authentication; extend session and JWT callbacks

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
+import { getUserById } from "./data/user";
 import { db } from "./lib/db";
 import authConfig from "./auth.config";
 
@@ -10,6 +11,29 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: {
     strategy: "jwt",
